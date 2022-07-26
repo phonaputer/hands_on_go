31d6dbf40acc4333e3317c4a5747f42f42ba9e80
phonaputer <njerome@uwalumni.com>
2022-07-26 13:05:03 +0900

    Add integration tests for prework assignment.

--- deployments/local/mysql/schema/04_test_users.sql
+++ deployments/local/mysql/schema/04_test_users.sql
@@ -1,4 +1,10 @@
 USE hands_on_go;
 
-INSERT INTO `users` (first_name, last_name, age, phone_number, phone_verification_status) 
-	VALUES ('Risa', 'Rakuten', 50, '+1-800-555-5555', 1);
+INSERT INTO `users` (id, first_name, last_name, age, phone_number, phone_verification_status)
+	VALUES (1, 'Risa', 'Rakuten', 50, '+1-800-555-5555', 1);
+
+INSERT INTO `users` (id, first_name, last_name, age, phone_number, phone_verification_status)
+    VALUES (2, 'Taro', 'Rakuten', 60, '+1-800-777-7777', 0);
+
+INSERT INTO `users` (id, first_name, last_name, age, phone_number, phone_verification_status)
+    VALUES (3, 'Quiz', 'Testersson', 30, '', 1);
