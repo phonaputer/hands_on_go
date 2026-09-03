42630a8060bf3ee497767f0d22c74f37e5e11810
phonaputer <njerome@uwalumni.com>
2026-09-03 10:10:21 +0900

    Write dummy HTTP handler.

--- user_application/internal/userapp/application.go
+++ user_application/internal/userapp/application.go
@@ -1,7 +1,26 @@
 package userapp
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func Run() {
-	fmt.Println("Hello, world!")
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: http.HandlerFunc(handleRequest),
+	}
+
+	err := s.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("I was called!")
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Thanks for calling!\n"))
 }
